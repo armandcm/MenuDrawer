e07e2178b22684b45e3a44e2040eafade501546f
Armandcm <armandocasells@gmail.com>
2023-10-05 21:37:15 +0200

    MenuDrawer

--- MenuDrawer/ViewController.swift
+++ MenuDrawer/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var openMenuButton: UIButton!
+    
+    var isMenuOpen = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        menuView.frame.origin.x = -menuView.frame.size.width
     }
 
-
+    @IBAction func toggleMenu(_ sender: Any) {
+        if isMenuOpen {
+            // Cierra el menú
+            UIView.animate(withDuration: 0.3) {
+                self.menuView.frame.origin.x = -self.menuView.frame.size.width
+            }
+        } else {
+            // Abre el menú
+            UIView.animate(withDuration: 0.3) {
+                self.menuView.frame.origin.x = 0
+            }
+        }
+        isMenuOpen = !isMenuOpen
+    }
 }
 
