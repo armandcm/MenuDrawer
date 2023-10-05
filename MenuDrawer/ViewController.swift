5415b5350c9a567306bcf0d573c785df9d254aaa
Armandcm <armandocasells@gmail.com>
2023-10-05 22:14:59 +0200

    modificaciones

--- MenuDrawer/ViewController.swift
+++ MenuDrawer/ViewController.swift
@@ -9,22 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBOutlet weak var menuView: UIView!
-    @IBOutlet weak var openMenuButton: UIButton!
+    var menuView: UIView!
+    let openMenuButton = UIButton(frame: CGRect(x: 20, y: 70, width: 100, height: 44))
     
     var isMenuOpen = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        menuView.frame.origin.x = -menuView.frame.size.width
+        
+        openMenuButton.backgroundColor = UIColor.black
+        openMenuButton.setTitle("Open Menu", for: .normal)
+        openMenuButton.addTarget(self, action: #selector(toggleMenu), for: .touchUpInside)
+        self.view.addSubview(openMenuButton)
+        
+        menuView = UIView(frame: CGRect(x: -self.view.frame.size.width, y: 120, width: 250, height: 700))
+        menuView.backgroundColor = UIColor.white // Puedes personalizar el color como desees
+        menuView.layer.cornerRadius = 15 // Ajusta el valor según sea necesario para obtener el redondeo deseado
+        menuView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+        menuView.clipsToBounds = false
+        menuView.layer.shadowColor = UIColor.black.cgColor // Color de la sombra
+        menuView.layer.shadowOpacity = 0.5 // Opacidad de la sombra (0.0 a 1.0)
+        menuView.layer.shadowOffset = CGSize(width: 2, height: 2) // Desplazamiento de la sombra
+        menuView.layer.shadowRadius = 4 // Radio de la sombra
+        self.view.addSubview(menuView)
+
     }
 
-    @IBAction func toggleMenu(_ sender: Any) {
+    @objc func toggleMenu() {
         if isMenuOpen {
             // Cierra el menú
             UIView.animate(withDuration: 0.3) {
-                self.menuView.frame.origin.x = -self.menuView.frame.size.width
+                self.menuView.frame.origin.x = -self.view.frame.size.width
             }
         } else {
             // Abre el menú
