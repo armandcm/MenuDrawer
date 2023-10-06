bcface0c07d6de391fbb9a24b79dd9180f7717b3
Armandcm <armandocasells@gmail.com>
2023-10-06 18:14:35 +0200

    mas de mas

--- MenuDrawer/ViewController.swift
+++ MenuDrawer/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     var menuView: UIView!
-    let topMenu = UIView(frame: CGRect(x: 10, y: 70, width: 373, height: 44))
-    let openMenuButton = UIButton(frame: CGRect(x: 20, y: 0, width: 44, height: 44))
-    let titApp = UILabel(frame: CGRect(x: 100, y: 0, width: 200, height: 44))
+    var logoMenu: UIView!
+    var logoCircle: UIView!
+    let topMenu = UIView(frame: CGRect(x: 4, y: 70, width: 385, height: 44))
+    let openMenuButton = UIButton(frame: CGRect(x: 20, y: 5, width: 34, height: 34))
+    let titApp = UILabel(frame: CGRect(x: 65, y: 0, width: 200, height: 44))
     
     var isMenuOpen = false
 
@@ -25,7 +27,7 @@ class ViewController: UIViewController {
         topMenu.layer.cornerRadius = 5
         topMenu.clipsToBounds = false
         
-        if let imagenFondo = UIImage(named: "iconmenu") {
+        if let imagenFondo = UIImage(named: "iconmenup") {
             openMenuButton.setBackgroundImage(imagenFondo, for: .normal)
         }
         openMenuButton.addTarget(self, action: #selector(toggleMenu), for: .touchUpInside)
@@ -41,15 +43,36 @@ class ViewController: UIViewController {
         
         self.view.addSubview(topMenu)
         
-        menuView = UIView(frame: CGRect(x: -self.view.frame.size.width, y: 120, width: 250, height: 700))
-        menuView.backgroundColor = UIColor.white // Puedes personalizar el color como desees
-        menuView.layer.cornerRadius = 15 // Ajusta el valor según sea necesario para obtener el redondeo deseado
+        menuView = UIView(frame: CGRect(x: -self.view.frame.size.width, y: 125, width: 250, height: 695))
+        menuView.backgroundColor = UIColor.white
+        menuView.layer.cornerRadius = 15
         menuView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
         menuView.clipsToBounds = false
-        menuView.layer.shadowColor = UIColor.black.cgColor // Color de la sombra
-        menuView.layer.shadowOpacity = 0.5 // Opacidad de la sombra (0.0 a 1.0)
-        menuView.layer.shadowOffset = CGSize(width: 2, height: 2) // Desplazamiento de la sombra
-        menuView.layer.shadowRadius = 4 // Radio de la sombra
+        menuView.layer.shadowColor = UIColor.black.cgColor
+        menuView.layer.shadowOpacity = 0.5
+        menuView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        menuView.layer.shadowRadius = 4
+        
+        logoMenu = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        logoMenu.backgroundColor = UIColor.orange
+        logoMenu.layer.cornerRadius = 15
+        logoMenu.layer.maskedCorners = [.layerMaxXMinYCorner]
+        logoMenu.clipsToBounds = true
+        
+        logoCircle = UIView(frame: CGRect(x: 10, y: 10, width: 230, height: 230))
+        logoCircle.backgroundColor = UIColor.white
+        logoCircle.layer.cornerRadius = logoCircle.frame.width / 2
+        logoCircle.clipsToBounds = true
+        
+        let logoImage = UIImageView(image: UIImage(named: "logosomlacostera"))
+        logoImage.contentMode = .scaleAspectFit
+        logoImage.frame = CGRect(x: 10, y: 0, width: 200, height: 200)
+        logoCircle.addSubview(logoImage)
+        
+        logoMenu.addSubview(logoCircle)
+        
+        menuView.addSubview(logoMenu)
+        
         self.view.addSubview(menuView)
 
     }
